9aaa736b796b2ad391d413c23c9942c7f6892dbb
Kuba <sekenuw@gmail.com>
2020-04-05 22:47:14 +0200

    Change annotation in TaskController

--- src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
@@ -6,13 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -21,14 +22,16 @@ class TaskController {
         this.repository = repository;
     }
 
-    //@RequestMapping(method = RequestMethod.GET, path = "/tasks") // jak tylko przychodzi request, ma on trafić do tej metody
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"}) //wchodzę w tą metodę, gdy nie użyłem metod sort,page,size
+
+    //@GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"}) //wchodzę w tą metodę, gdy nie użyłem metod sort,page,size
+    @RequestMapping(method = RequestMethod.GET, path = "/tasks", params = {"!sort", "!page", "!size"}) // jak tylko przychodzi request, ma on trafić do tej metody
     ResponseEntity<List<Task>> readAllTasks() { //wypisuje tylko listę tasków
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping(value = "/tasks") //wchodzę tu jak użyłem metod sort/page/pageable
+    //@GetMapping(value = "/tasks") //wchodzę tu jak użyłem metod sort/page/pageable
+    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
@@ -40,7 +43,8 @@ class TaskController {
 //        return ResponseEntity.ok(repository.findAll(page));
 //    }
 
-    @GetMapping("/tasks/{id}")
+//    @GetMapping("/tasks/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
     ResponseEntity<Task> readTask(@PathVariable int id) {
         return repository.findById(id)
             .map(task -> ResponseEntity.ok(task))
@@ -57,7 +61,8 @@ class TaskController {
 //        }
 //    }
 
-    @PutMapping("/tasks/{id}")
+    //@PutMapping("/tasks/{id}")
+    @RequestMapping( method = RequestMethod.PUT, path = "/tasks/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -83,7 +88,8 @@ class TaskController {
 //        }
 //    }
 
-    @PostMapping("/tasks")
+    //@PostMapping("/tasks")
+    @RequestMapping(method = RequestMethod.POST, path = "/tasks")
     ResponseEntity<Task> createTask (@RequestBody @Valid Task toCreate) {
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
