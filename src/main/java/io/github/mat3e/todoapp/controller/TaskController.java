5230ba0bc2c0e132d753fa5ceb7f45f9bc30cf64
Kuba <sekenuw@gmail.com>
2020-04-06 22:51:10 +0200

    Add H2 Database console

--- src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
@@ -6,14 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@Controller
+@RestController
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -94,4 +93,6 @@ class TaskController {
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
+
+
 }
