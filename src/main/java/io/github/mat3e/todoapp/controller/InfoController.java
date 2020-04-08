2fa7bd27f0cad445a4d86c0b11d709b197232898
Kuba <sekenuw@gmail.com>
2020-04-08 22:50:54 +0200

    Add properties

--- src/main/java/io/github/mat3e/todoapp/controller/InfoController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/InfoController.java
@@ -1,4 +1,27 @@
 package io.github.mat3e.todoapp.controller;
 
-public class InfoController {
+import io.github.mat3e.todoapp.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class InfoController {
+    private DataSourceProperties dataSource; // w ten sposob unikam literówek przy wpisywaniu property
+    private static TaskConfigurationProperties myProp;
+
+    InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/info/url")
+    String url() {
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp() {
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
 }
