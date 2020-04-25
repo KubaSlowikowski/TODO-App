4c52792c12141b1e4f96cc22b6dc7491a4211156
Kuba <sekenuw@gmail.com>
2020-04-25 20:01:12 +0200

    Add filter and interceptor

--- src/main/java/io/github/mat3e/todoapp/controller/InfoController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/InfoController.java
@@ -3,9 +3,11 @@ package io.github.mat3e.todoapp.controller;
 import io.github.mat3e.todoapp.TaskConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/info")
 class InfoController {
     private DataSourceProperties dataSource; // w ten sposob unikam literówek przy wpisywaniu property
     private static TaskConfigurationProperties myProp;
@@ -15,12 +17,12 @@ class InfoController {
         this.myProp = myProp;
     }
 
-    @GetMapping("/info/url")
+    @GetMapping("/url")
     String url() {
         return dataSource.getUrl();
     }
 
-    @GetMapping("/info/prop")
+    @GetMapping("/prop")
     boolean myProp() {
         return myProp.getTemplate().isAllowMultipleTasks();
     }
--- src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
+++ src/main/java/io/github/mat3e/todoapp/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -24,45 +25,46 @@ class TaskController {
 
 
     //@GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"}) //wchodzę w tą metodę, gdy nie użyłem metod sort,page,size
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks", params = {"!sort", "!page", "!size"}) // jak tylko przychodzi request, ma on trafić do tej metody
+    @RequestMapping(method = RequestMethod.GET, params = {"!sort", "!page", "!size"}) // jak tylko przychodzi request, ma on trafić do tej metody
     ResponseEntity<List<Task>> readAllTasks() { //wypisuje tylko listę tasków
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
     //@GetMapping(value = "/tasks") //wchodzę tu jak użyłem metod sort/page/pageable
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @RequestMapping(method = RequestMethod.GET)
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-//    @GetMapping(value = "/tasks") //wchodzę tu jak użyłem metod sort/page/pageable
-//    ResponseEntity<?> readAllTasks(Pageable page) {
-//        logger.info("Custom pageable");
-//        return ResponseEntity.ok(repository.findAll(page));
-//    }
-
 //    @GetMapping("/tasks/{id}")
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     ResponseEntity<Task> readTask(@PathVariable int id) {
         return repository.findById(id)
             .map(task -> ResponseEntity.ok(task))
             .orElse(ResponseEntity.notFound().build());
     }
 
-    //    @GetMapping("/tasks/{id}")
-//    ResponseEntity<Task> readTask(@PathVariable int id) {
-//        Optional<Task> optionalTask = repository.findById(id);
-//        if (!optionalTask.isPresent())
-//            return new ResponseEntity<>(HttpStatus.CONFLICT);
-//        else {
-//            return ResponseEntity.ok(optionalTask.get());
-//        }
+//    @GetMapping(value = "/search/done", produces = MediaType.APPLICATION_JSON_VALUE) //gdy żądanie HTTP zawiera JSON
+//    String foo() {
+//        return "";
+//    }
+//    @GetMapping(value = "/search/done", produces = MediaType.TEXT_XML_VALUE) //gdy żądanie HTTP zawiera XML
+//    String bar() {
+//        return "";
 //    }
 
+    @GetMapping("/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state) {
+        return ResponseEntity.ok(
+                repository.findByDone(state)
+        );
+    }
+
+
     //@PutMapping("/tasks/{id}")
-    @RequestMapping( method = RequestMethod.PUT, path = "/tasks/{id}")
+    @RequestMapping( method = RequestMethod.PUT, path = "/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -92,14 +94,14 @@ class TaskController {
 //    }
 
     //@PostMapping("/tasks")
-    @RequestMapping(method = RequestMethod.POST, path = "/tasks")
+    @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<Task> createTask (@RequestBody @Valid Task toCreate) {
         Task result = repository.save(toCreate);
         return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @Transactional //zmiana zostanie zarejestrowana w bazie danych
-    @RequestMapping(method = RequestMethod.PATCH, path = "/tasks/{id}")
+    @RequestMapping(method = RequestMethod.PATCH, path = "/{id}")
     public ResponseEntity<?> toogleTask(@PathVariable int id) { //warunek konieczny - metoda publiczna
         if(!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
